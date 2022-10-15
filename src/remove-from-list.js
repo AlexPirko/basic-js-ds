9c0ed367a0928a57736a27a2382940df87f14a9c
Alex <youzhny.foto@gmail.com>
2022-10-15 10:12:10 +0300

    basic-js-ds done!

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function removeKFromList(l, k) {
+    
+  let node = l;
+
+
+  while (node.next) {
+    if (node.next.value === k) {
+      node.next = node.next.next;
+    } else {
+      current = node.next;
+    }
+  }
+    while (l.value === k) {
+      l = l.next;
+    }
+    
+  return l;
 }
 
 module.exports = {
